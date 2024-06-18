292e171f3326a1f75ba05a035e82c6e6bc426f84
Rui Araújo <ruiaraujo012@gmail.com>
2024-06-18 18:40:08 +0100

    feat(ci): add semantic-release

---
+++ release.config.mjs
@@ -0,0 +1,78 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: [
+    "main",
+    "next",
+    {
+      name: "dev",
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+        },
+        preset: "angular",
+        releaseRules: [
+          {
+            release: false,
+            scope: "wip",
+          },
+          {
+            release: false,
+            scope: "WIP",
+          },
+          {
+            release: false,
+            scope: "no-release",
+          },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+        },
+        preset: "angular",
+        writerOpts: {
+          commitsSort: ["subject", "scope"],
+        },
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    // [
+    //   "@semantic-release/exec",
+    //   {
+    //     prepareCmd: "sh ./scripts/update-version.sh ${nextRelease.version}",
+    //   },
+    // ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "pnpm-lock.yaml", "CHANGELOG.md", "dist"],
+      },
+    ],
+    "@semantic-release/github",
+  ],
+  publishConfig: {
+    access: "restricted",
+  },
+};
