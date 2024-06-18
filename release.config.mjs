3a5d7a04fce9e800c57faf151e949fb8afcb93f7
Rui Araújo <ruiaraujo012@gmail.com>
2024-06-18 19:05:32 +0100

    fix(ci): ci

--- release.config.mjs
+++ release.config.mjs
@@ -75,7 +75,7 @@ export default {
       "@semantic-release/github",
       {
         assets: [
-          { path: "dist", label: "Dist" },
+          { path: "dist/*", label: "dist" },
           { path: "CHANGELOG.md", label: "Changelog" },
         ],
       },
